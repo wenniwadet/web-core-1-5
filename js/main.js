425a6e46c9d2d4ea91a371fd3a160746fdec7927
wenniwadet <wenniwadit@gmail.com>
2023-02-25 21:58:10 +0500

    load

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
+  slidesPerView: 1.25,
+  spaceBetween: 16,
 
   pagination: {
     el: ".swiper-pagination",
+    bulletActiveClass: "bullet--active",
+    horizontalClass: "bullet--gap",
+  },
+});
+
+let pagination = document.querySelector(".swiper-pagination");
+
+if (document.documentElement.clientWidth >= 768) {
+  swiper.destroy();
+  pagination.classList.add("hidden");
+}
+
+let buttonSnowBrands = document.querySelector(".brands__spoiler");
+let brandsList = document.querySelector(".brands__list");
+console.log(brandsList);
+
+buttonSnowBrands.addEventListener("click", function () {
+  brandsList.classList.toggle("brands__list--snow-all");
+  buttonSnowBrands.classList.toggle("spoiler--active");
+  if (buttonSnowBrands.classList.contains("spoiler--active")) {
+    buttonSnowBrands.textContent = "Скрыть";
+  } else {
+    buttonSnowBrands.textContent = "Показать все";
   }
 });
